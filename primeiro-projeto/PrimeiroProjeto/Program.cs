b93b328aebb20db1fd80ae99ef711cf41d5414c6
apartigor <contact.jojigor@gmail.com>
2025-02-26 21:32:22 -0300

    aula_1

---
+++ primeiro-projeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Carro carro = new Carro("Fusca", 1980);
+        carro.ExibirDetalhes();
+    }
+}
+
+
+class Carro
+{
+
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public Carro(string modelo, int ano)
+    {
+        Modelo = modelo;
+        Ano = ano;
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Modelo: {Modelo}, Ano: {Ano} ");
+    }
+}
+
